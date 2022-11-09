141f97dce8bcb036741a6ec5a1e1bd0d6663d938
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-09 19:23:13 +0530

    Create text.py

---
+++ text.py
@@ -0,0 +1,4 @@
+START_TXT = """<b>Hello {m.from_user.mention}👋,
+I am Link Bypasser Bot.
+I Will Help You To Bypass Links Without See Dozens Of Ads. 
+Made With ❤️ by [*Shivam*](https://t.me/Legend_Shivam_7)"""
