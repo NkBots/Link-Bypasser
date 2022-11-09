96b56636cba2cbb7f35cbe619fb779b03f4a183c
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-09 19:28:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,77 @@ api_hash = os.environ.get("HASH", "")
 api_id = os.environ.get("ID", "")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token) 
 
+bipinkrish
+/
+Link-Bypasser-Bot
+Public
+Code
+Issues
+2
+Pull requests
+Actions
+Projects
+Security
+Insights
+Link-Bypasser-Bot/main.py
+@bipinkrish
+bipinkrish Error fix
+ 1 contributor
+94 lines (76 sloc)  3.6 KB
+import pyrogram
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+import bypasser
+import os
+import ddl
+import requests
+import threading
+from texts import HELP_TEXT
+from bypasser import ddllist
+
+
+# bot
+bot_token = os.environ.get("TOKEN", "")
+api_hash = os.environ.get("HASH", "") 
+api_id = os.environ.get("ID", "")
+app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
+
+
+# loop thread
+def loopthread(message):
+    urls = []
+    for ele in message.text.split():
+        if "http://" in ele or "https://" in ele:
+            urls.append(ele)
+    if len(urls) == 0:
+        return
+
+    if bypasser.ispresent(ddllist,urls[0]):
+        msg = app.send_message(message.chat.id, "⚡ __generating...__", reply_to_message_id=message.id)
+    else:
+        if urls[0] in "https://olamovies" or urls[0] in "https://psa.pm/":
+            msg = app.send_message(message.chat.id, "🔎 __this might take some time...__", reply_to_message_id=message.id)
+        else:
+            msg = app.send_message(message.chat.id, "🔎 __bypassing...__", reply_to_message_id=message.id)
+
+    link = ""
+    for ele in urls:
+        if bypasser.ispresent(ddllist,ele):
+            try: temp = ddl.direct_link_generator(ele)
+            except Exception as e: temp = "**Error**: " + str(e)
+        else:    
+            try: temp = bypasser.shortners(ele)
+            except Exception as e: temp = "**Error**: " + str(e)
+        print("bypassed:",temp)
+        link = link + temp + "\n\n"
+        
+    try: app.edit_message_text(message.chat.id, msg.id, f'__{link}__', disable_web_page_preview=True)
+    except: app.edit_message_text(message.chat.id, msg.id, "__Failed to Bypass__")
+
+
+
+
 @app.on_message(filters.command(["start"]))
 def send_help(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
     app.send_message(message.chat.id, START_TXT, reply_to_message_id=message.id, disable_web_page_preview=True)
