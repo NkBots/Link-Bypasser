b23ee89552b391f2d151e19d27a55f3655f5a61f
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-09 23:30:17 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -36,3 +36,21 @@ def mdisk(url):
         return res["url"]
     else:
         return res["msg"]
+
+# droplink
+
+def droplink(url):
+    api = "https://api.emilyx.in/api/bypass"
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    resp = client.get(url)
+    if resp.status_code == 404:
+        return "File not found/The link you entered is wrong!"
+    try:
+        resp = client.post(api, json={"type": "droplink", "url": url})
+        res = resp.json()
+    except BaseException:
+        return "API UnResponsive / Invalid Link !"
+    if res["success"] is True:
+        return res["url"]
+    else:
+        return res["msg"]
