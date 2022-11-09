07578d1018e931c6ae09bd4be84b606e4a9fd4bc
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-09 19:21:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,23 @@ bot_token = os.environ.get("TOKEN", "")
 api_hash = os.environ.get("HASH", "") 
 api_id = os.environ.get("ID", "")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token) 
+
+@app.on_message(filters.command(["start"]))
+def send_help(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    app.send_message(message.chat.id, START_TXT, reply_to_message_id=message.id, disable_web_page_preview=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Bot Starting")
+app.run()
