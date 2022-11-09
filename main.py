afab53568ebb627cc8d735f96673a47824fc3169
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-09 19:18:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyrogram
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+import bypasser
+import os
+import ddl
+import requests
+import threading
+from texts import
+from bypasser import shortlist
+
+
+# bot
+bot_token = os.environ.get("TOKEN", "")
+api_hash = os.environ.get("HASH", "") 
+api_id = os.environ.get("ID", "")
+app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token) 
