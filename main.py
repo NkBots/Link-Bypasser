ff4762c724f469ef282066e099acf41419027c91
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-11 18:25:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 import bypasser
 import os
+import ddl
 import requests
 import threading
-from text import START_TXT
+from texts import HELP_TEXT
 from bypasser import ddllist
+
 
 # bot
 bot_token = os.environ.get("TOKEN", "")
 api_hash = os.environ.get("HASH", "") 
 api_id = os.environ.get("ID", "")
-app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token) 
+app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
 
 
 # loop thread
@@ -48,11 +50,17 @@ def loopthread(message):
     except: app.edit_message_text(message.chat.id, msg.id, "__Failed to Bypass__")
 
 
-
-
+# start command
 @app.on_message(filters.command(["start"]))
+def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
+    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Link-Bypasser-Bot")]]), reply_to_message_id=message.id)
+
+
+# help command
+@app.on_message(filters.command(["help"]))
 def send_help(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    app.send_message(message.chat.id, START_TXT, reply_to_message_id=message.id, disable_web_page_preview=True)
+    app.send_message(message.chat.id, HELP_TEXT, reply_to_message_id=message.id, disable_web_page_preview=True)
 
 
 # links
@@ -62,14 +70,25 @@ def receive(client: pyrogram.client.Client, message: pyrogram.types.messages_and
     bypass.start()
 
 
+# doc thread
+def docthread(message):
+    if message.document.file_name.endswith("dlc"):
+        msg = app.send_message(message.chat.id, "🔎 __bypassing...__", reply_to_message_id=message.id)
+        print("sent DLC file")
+        sess = requests.session()
+        file = app.download_media(message)
+        dlccont = open(file,"r").read()
+        link = bypasser.getlinks(dlccont,sess)
+        app.edit_message_text(message.chat.id, msg.id, f'__{link}__')
+        os.remove(file)
+
+# doc
+@app.on_message(filters.document)
+def docfile(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    bypass = threading.Thread(target=lambda:docthread(message),daemon=True)
+    bypass.start()
 
 
-
-
-
-
-
-
-
+# server loop
 print("Bot Starting")
 app.run()
