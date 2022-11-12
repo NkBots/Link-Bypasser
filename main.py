c423c289e86e200b7c5647c58447fa26844bcdd2
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-12 17:30:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,34 @@ async def link_handler(bot, message):
 
 
 
+def mdis_k(urlx):
+    scraper = cloudscraper.create_scraper(interpreter="nodejs", allow_brotli=False)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
+    }
+    apix = f"http://x.egraph.workers.dev/?param={urlx}"
+    response = scraper.get(apix, headers=headers)
+    query = response.json()
+    return query
+
+
+def mdisk(url):
+    check = re.findall(r"\bhttps?://.*mdisk\S+", url)
+    if not check:
+        textx = f"**Invalid link**"
+        return textx
+    else:
+        try:
+            fxl = url.split("/")
+            urlx = fxl[-1]
+            uhh = mdis_k(urlx)
+            duration = {uhh["duration"]}
+            text = f'**📂 Title** : `{uhh["filename"]}`\n\n📥 **Download URL (If mxv Present in link then it support only MX player\n\nIf dash, mpd, M3U8, hls present in link then it support all player)** :- {uhh["source"]}\n\n📤 **Download URL (Support Only MX Player)** :- {uhh["download"]}\n\n💎 **Uploader User ID** :- `{uhh["from"]}`\n\n💠 **Uploader User Name** :- `@{uhh["display_name"]}`\n\n📹 **Video Width** :- `{uhh["width"]}`\n\n🎞 **Video Height** :- {uhh["height"]}\n\n📦 **Video Duration** :- `{uhh["duration"]}s`\n\n📊 **Video Size** :- `{uhh["size"]}kb`'
+            return text
+        except ValueError:
+            textx = f"The Content is Deleted."
+            return textx
+
 
 # server loop
 print("Bot Starting")
